d3b71a0e3e77b1333fc87ef55c02db33658e185f
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-09-27 19:33:27 +0530

    import java.util.Scanner; class Patt {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=0;i<n;i++)         {             for(j=0;j<n;j++)             {                 if(i==j)                 {                     System.out.printf("0");                 }                 else                 {                     System.out.printf("x");                 }
                }
                
                System.out.println();
            }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -3,14 +3,21 @@ class Patt
 {
     public static void main(String args[])
     {
-        int n,i,j;
         Scanner sc=new Scanner(System.in);
+        int n,i,j;
         n=sc.nextInt();
-        for( i=0;i<n;i++)
+        for(i=0;i<n;i++)
         {
-            for( j=n;j>0;j--)
+            for(j=0;j<n;j++)
             {
-                System.out.printf("%d ",j);
+                if(i==j)
+                {
+                    System.out.printf("0");
+                }
+                else
+                {
+                    System.out.printf("x");
+                }
                 
             }
             
