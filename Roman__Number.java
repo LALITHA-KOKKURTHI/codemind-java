ac520fb40723894d6c329eeb0f03d2320e9a4a09
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-10-12 11:43:55 +0530

    import java.util.Scanner; class Solution {     public static void integerToRoman(int n)     {         int[] val={1000,900,500,400,100,90,50,40,10,9,5,4,1};         String[] rom={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};         StringBuilder I=new StringBuilder();         for(int i=0;i<val.length;i++)         {             while(n>=val[i])             {                 n-=val[i];                 I.append(rom[i]);             }         }         System.out.println(I.toString());     }     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         integerToRoman(n);     } }

---
+++ Roman__Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void integerToRoman(int n)
+    {
+        int[] val={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] rom={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder I=new StringBuilder();
+        for(int i=0;i<val.length;i++)
+        {
+            while(n>=val[i])
+            {
+                n-=val[i];
+                I.append(rom[i]);
+            }
+        }
+        System.out.println(I.toString());
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        integerToRoman(n);
+    }
+}
